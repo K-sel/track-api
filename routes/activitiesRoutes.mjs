1f4492f537d4b9bdc50d16af15522dd2c91f8d82
Vince <jpinard@bluewin.ch>
2025-11-17 10:00:32 +0100

    auth branch created

--- routes/activitiesRoutes.mjs
+++ routes/activitiesRoutes.mjs
@@ -1,12 +1,9 @@
 import express from "express";
-import activitiesController from "../controllers/activitiesController.mjs";
 
 const router = express.Router();
 
 router.get("/", function (req, res, next) {
   res.send("Get from Activites routes");
 });
-
-router.get("/test", new activitiesController)
 
 export default router;
