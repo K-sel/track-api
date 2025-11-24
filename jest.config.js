21b42d63d979d6a0ca468eb46586cf6c1c94b838
Jonathan <jpinard@bluewin.ch>
2025-11-24 11:41:14 +0100

    middlwares unit tests done + implemented coverage reporting

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,13 @@
 export default {
-  testEnvironment: 'node',
-  testPathIgnorePatterns: [
-    '/node_modules/',
-    '/spec/unit/'
-  ]
+  testEnvironment: "node",
+  testPathIgnorePatterns: ["/node_modules/"],
+  collectCoverageFrom: [
+    "**/*.mjs",
+    "!**/node_modules/**",
+    "!**/spec/**",
+    "!**/coverage/**",
+    "!**/start.mjs/**",
+    "!**/app.mjs/**",
+  ],
+  coverageReporters: ["text", "lcov"],
 };
--- spec/helpers/utils.js
+++ spec/helpers/utils.js
@@ -12,6 +12,23 @@ const signJwt = promisify(jwt.sign);
 export function generateValidJwt(user) {
   // Génère un JWT valide qui expire dans 7 jours
   const exp = Math.floor((new Date().getTime() + 7 * 24 * 3600 * 1000) / 1000);
+
+  const claims = {
+    sub: user._id.toString(),
+    exp: exp
+  };
+  return signJwt(claims, process.env.SECRET_KEY);
+}
+
+/**
+ * Génère un JWT expiré pour les tests
+ * @param {Object} user - L'utilisateur pour lequel générer le token
+ * @returns {Promise<string>} Le token JWT signé
+ */
+export function generateExpiredJwt(user) {
+  // Génère un JWT valide qui est expiré
+  const exp = Math.floor((new Date().getTime() - 7 * 24 * 3600 * 1000) / 1000);
+  
   const claims = {
     sub: user._id.toString(),
     exp: exp
